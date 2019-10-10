bd6e3519dc161f782f98f1e57665cf1356a66f81
Ryan Mitchell <rtmitchell@google.com>
2019-10-10 08:15:55 -0700

    sdm660-common: Use component-override to configure crosshatch Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default
    on crosshatch devices.
    
    Remove usages of profile_supported_sap from overlays as they no
    longer configure the enable state of .sap.SapService.
    
    Bug: 135048762
    Test: adb shell pm query-services \
          com.android.bluetooth/.sap.SapService
    
    Change-Id: I89ba2aa61492f65b269849597282594507d2fdc3

--- sdm660.mk
+++ sdm660.mk
@@ -148,6 +148,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
