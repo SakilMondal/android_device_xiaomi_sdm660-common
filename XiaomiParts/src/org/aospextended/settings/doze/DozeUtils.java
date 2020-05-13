4c3da1417db61576587b4cb2b2b766729607be41
SakilMondal <mondalsakil@gmail.com>
2020-05-13 13:24:31 +0530

    sdm660-common: Remove lineageos tit-bits
    Change-Id: Ic9cdabd7d5e3e72864e229c934f11d6957397fc0
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- XiaomiParts/src/org/aospextended/settings/doze/DozeUtils.java
+++ XiaomiParts/src/org/aospextended/settings/doze/DozeUtils.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.doze;
+package org.aospextended.settings.doze;
 
 import android.content.Context;
 import android.content.Intent;
