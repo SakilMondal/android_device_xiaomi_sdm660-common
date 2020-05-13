e2cf1e4e7b02471cb8633c554952c911ed017682
SakilMondal <mondalsakil@gmail.com>
2020-05-13 13:24:31 +0530

    sdm660-common: Remove lineageos tit-bits
    Change-Id: Ic9cdabd7d5e3e72864e229c934f11d6957397fc0
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- XiaomiParts/src/org/aospextended/settings/BootCompletedReceiver.java
+++ XiaomiParts/src/org/aospextended/settings/BootCompletedReceiver.java
@@ -15,14 +15,14 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings;
+package org.aospextended.settings;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineageos.settings.doze.DozeUtils;
+import org.aospextended.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
