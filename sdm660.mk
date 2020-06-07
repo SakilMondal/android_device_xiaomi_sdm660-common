a1911f4a1a3c4d9ecc994b177c9ef21cdb692849
ShubhamB <shubhamprince111@gmail.com>
2020-06-07 13:43:37 +0530

    sdm660-common: XiaomiParts: Add USB Fastcharge Control
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
    Change-Id: Ia54d71e51ccdd94c0236b9959b84b196e52a5388
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -273,6 +273,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.xiaomiparts.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
