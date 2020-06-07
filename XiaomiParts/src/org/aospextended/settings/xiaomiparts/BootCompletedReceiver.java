3a8beb8b522f7d51a69649922920ca294d7a4684
ShubhamB <shubhamprince111@gmail.com>
2020-06-07 16:46:26 +0530

    sdm660-common: XiaomiParts: Add KCal support
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
    Change-Id: I9aaf34a464c9d8dfe553f59cfec9cb34c69e01b0
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- XiaomiParts/src/org/aospextended/settings/xiaomiparts/BootCompletedReceiver.java
+++ XiaomiParts/src/org/aospextended/settings/xiaomiparts/BootCompletedReceiver.java
@@ -24,9 +24,10 @@ import android.provider.Settings;
 import android.util.Log;
 
 import org.aospextended.settings.xiaomiparts.doze.DozeUtils;
+import org.aospextended.settings.xiaomiparts.kcal.Utils;
 import org.aospextended.settings.xiaomiparts.vibration.VibratorStrengthPreference;
 
-public class BootCompletedReceiver extends BroadcastReceiver {
+public class BootCompletedReceiver extends BroadcastReceiver implements Utils {
 
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
@@ -36,6 +37,33 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
         VibratorStrengthPreference.restore(context);
+
+        if (Settings.Secure.getInt(context.getContentResolver(), PREF_ENABLED, 0) == 1) {
+            FileUtils.setValue(KCAL_ENABLE, Settings.Secure.getInt(context.getContentResolver(),
+                    PREF_ENABLED, 0));
+
+            String rgbValue = Settings.Secure.getInt(context.getContentResolver(),
+                    PREF_RED, RED_DEFAULT) + " " +
+                    Settings.Secure.getInt(context.getContentResolver(), PREF_GREEN,
+                            GREEN_DEFAULT) + " " +
+                    Settings.Secure.getInt(context.getContentResolver(), PREF_BLUE,
+                            BLUE_DEFAULT);
+
+            FileUtils.setValue(KCAL_RGB, rgbValue);
+            FileUtils.setValue(KCAL_MIN, Settings.Secure.getInt(context.getContentResolver(),
+                    PREF_MINIMUM, MINIMUM_DEFAULT));
+            FileUtils.setValue(KCAL_SAT, Settings.Secure.getInt(context.getContentResolver(),
+                    PREF_GRAYSCALE, 0) == 1 ? 128 :
+                    Settings.Secure.getInt(context.getContentResolver(),
+                            PREF_SATURATION, SATURATION_DEFAULT) + SATURATION_OFFSET);
+            FileUtils.setValue(KCAL_VAL, Settings.Secure.getInt(context.getContentResolver(),
+                    PREF_VALUE, VALUE_DEFAULT) + VALUE_OFFSET);
+            FileUtils.setValue(KCAL_CONT, Settings.Secure.getInt(context.getContentResolver(),
+                    PREF_CONTRAST, CONTRAST_DEFAULT) + CONTRAST_OFFSET);
+            FileUtils.setValue(KCAL_HUE, Settings.Secure.getInt(context.getContentResolver(),
+                    PREF_HUE, HUE_DEFAULT));
+        }
+
         FileUtils.setValue(XiaomiParts.USB_FASTCHARGE_PATH, Settings.Secure.getInt(context.getContentResolver(),
                 XiaomiParts.PREF_USB_FASTCHARGE, 1));
     }
