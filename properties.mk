83cbf4c9b53625b23b3c3a22556e527fcdc83387
SakilMondal <mondalsakil@gmail.com>
2020-06-21 13:49:47 +0530

    sdm660-common: Disable default color mode
    * Changing display default color mode property value from 1 to 0.
    
    Change-Id: Id15914127751b0bcc5347743aabdfa0c480379a3
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- properties.mk
+++ properties.mk
@@ -153,7 +153,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.wcg_composition_dataspace=143261696 \
     ro.vendor.display.cabl=0 \
     vendor.display.disable_skip_validate=1 \
-    vendor.display.enable_default_color_mode=1 \
+    vendor.display.enable_default_color_mode=0 \
     vendor.gralloc.enable_fb_ubwc=1
 
 # FRP
