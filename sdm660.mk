3c5a8325d20fc36c6422f506ad899ebd3329c80e
Demon000 <demonsingur@gmail.com>
2020-09-16 23:54:41 +0530

    sdm660-common: Add shim for libdpmframework
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -433,6 +433,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Shims
+PRODUCT_PACKAGES += \
+    libcutils_shim
+
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
