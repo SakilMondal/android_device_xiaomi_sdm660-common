eced9f660b406c7d9551657db752b33384ff29fb
OdSazib <odsazib@gmail.com>
2020-10-19 11:38:34 +0600

    sdm660-common: Fix audio recording in video mode These files were pulled from one magisk module which fixes: - Low audio while recording video - Disorted audio and noise - Can record clear and loud audio afterward
    Change-Id: I187647f2e0475f780bdebc3abc4ae4d197b7be5e

--- sdm660.mk
+++ sdm660.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio_amplifier.sdm660 \
-    audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
