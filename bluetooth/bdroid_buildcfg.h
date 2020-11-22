d183e86cf6d2b1c906e1c8839659277bd0d36dcd
SakilMondal <mondalsakil@gmail.com>
2020-11-22 13:36:40 +0530

    Revert "sdm660: update bdroid_buildcfg values from caf"
    This reverts commit 89aa1b7e069703e4285ed87b11469ba29d82bc49.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,43 +21,13 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
-#include <stdint.h>
-#pragma push_macro("PROPERTY_VALUE_MAX")
-
-#include <cutils/properties.h>
-#include <string.h>
-
-static inline const char* BtmGetDefaultName()
-{
-    char product_model[PROPERTY_VALUE_MAX];
-    property_get("ro.product.model", product_model, "");
-
-    if (strstr(product_model, "Mi A2"))
-        return "Mi A2";
-    if (strstr(product_model, "MI 6X"))
-        return "MI 6X";
-    if (strstr(product_model, "MI PAD 4"))
-        return "MI PAD 4";
-    if (strstr(product_model, "MI PAD 4 PLUS"))
-        return "MI PAD 4 PLUS";
-
-    // Fallback to ro.product.model
-    return "";
-}
-#undef PROPERTY_VALUE_MAX
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
-#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define GATT_MAX_PHY_CHANNEL  10
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
 
-#define AVDT_NUM_SEPS 35
-#pragma pop_macro("PROPERTY_VALUE_MAX")
-
+/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
+#define AVDT_NUM_SEPS 12
 #endif
