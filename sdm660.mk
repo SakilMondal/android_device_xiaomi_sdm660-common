ad07f31898c2240227fdd064be89b080e7a89c18
sbwml <admin@sbwml.net>
2021-04-10 01:37:12 +0000

    [SQUASH]sdm660-common: wfd bringup
    wfd blobs from phoenix_sprout:11/RKQ1.201123.002/210641533e0c8:user/release-keys
    
    Co-authored-by: srfarias <erikofarias@gmail.com>
    Change-Id: I48c860f93f530b94e01ebfdfb7d45a430c307896

--- sdm660.mk
+++ sdm660.mk
@@ -174,9 +174,11 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
     libtinyxml \
     libqdMetaData \
-    libqdMetaData.system
+    libqdMetaData.vendor
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
@@ -188,7 +190,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9_vendor
 
 # DPM
 PRODUCT_PACKAGES += \
@@ -432,7 +435,8 @@ PRODUCT_COPY_FILES += \
 # WiFi Display
 PRODUCT_PACKAGES += \
     libaacwrapper \
-    libnl
+    libnl \
+    libstagefright_enc_common
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
